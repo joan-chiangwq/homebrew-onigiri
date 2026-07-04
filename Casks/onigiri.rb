8617a9fe522b6fac21eff216098a8446f813caa3
joan-chiangwq <joanchiang03@gmail.com>
2026-07-04 21:57:49 +0800

    Point at on-igiri-releases; fix sha256 and deprecated macos dep syntax

--- Casks/onigiri.rb
+++ Casks/onigiri.rb
@@ -1,11 +1,11 @@
 cask "onigiri" do
   version "1.0"
-  sha256 "6f664ceedea1771d9ff058a7554314cfee827616a5e6a87e8a5e8a478d768b32"
+  sha256 "ba7c639909b64df604f18f600a89de9ea930b29a86150862af6d5f51294c3b98"
 
-  url "https://github.com/joan-chiangwq/on-igiri/releases/download/v#{version}/Onigiri-#{version}.dmg"
+  url "https://github.com/joan-chiangwq/on-igiri-releases/releases/download/v#{version}/Onigiri-#{version}.dmg"
   name "Onigiri"
   desc "Menu bar app that keeps your Mac awake"
-  homepage "https://github.com/joan-chiangwq/on-igiri"
+  homepage "https://github.com/joan-chiangwq/on-igiri-releases"
 
   depends_on macos: :big_sur
 
