b1ad409a4e6956d3fa5580a130fd1c9a155363f8
joan-chiangwq <joanchiang03@gmail.com>
2026-07-04 22:06:40 +0800

    Update for on-igiri-releases -> onigiri-releases rename

--- Casks/onigiri.rb
+++ Casks/onigiri.rb
@@ -2,10 +2,10 @@ cask "onigiri" do
   version "1.0"
   sha256 "ba7c639909b64df604f18f600a89de9ea930b29a86150862af6d5f51294c3b98"
 
-  url "https://github.com/joan-chiangwq/on-igiri-releases/releases/download/v#{version}/Onigiri-#{version}.dmg"
+  url "https://github.com/joan-chiangwq/onigiri-releases/releases/download/v#{version}/Onigiri-#{version}.dmg"
   name "Onigiri"
   desc "Menu bar app that keeps your Mac awake"
-  homepage "https://github.com/joan-chiangwq/on-igiri-releases"
+  homepage "https://github.com/joan-chiangwq/onigiri-releases"
 
   depends_on macos: :big_sur
 
